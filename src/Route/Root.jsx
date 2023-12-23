af39b642fe8c464ed3b487b542154b8bffc774b6
Digontha <digonthodj@gmail.com>
2023-12-23 19:42:37 +0600

    implement animation

--- src/Route/Root.jsx
+++ src/Route/Root.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../Shared/Navbar';
+import Loader from '../Components/Loader/Loader';
 
 const Root = () => {
-    return (
-      <>
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Let create async method to fetch fake data
+  useEffect(() => {
+    const fakeDataFetch = () => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 4000);
+    };
+
+    fakeDataFetch();
+  }, []);
+
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <>
       <div>
         <Navbar></Navbar>
       </div>
-      
-      </>
-    );
+
+    </>
+  );
+
 };
 
 export default Root;
