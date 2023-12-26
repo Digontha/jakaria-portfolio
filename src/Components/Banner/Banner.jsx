f34aa965a4104c3941f60faca1198464ad749d34
Digontha <digonthodj@gmail.com>
2023-12-26 14:59:38 +0600

    create some design

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,50 +1,47 @@
-// Banner.js
-import React, { useEffect, useState } from 'react';
-import img from "../../../public/jakaria-removebg-preview.png";
 import "./Banner.css";
 import { Link } from 'react-router-dom';
-import {  FaFacebook, FaInstagram,  FaWhatsapp } from "react-icons/fa";
-
+import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import TypeIt from 'typeit';
+import React, { useEffect } from "react";
 
 const Banner = () => {
-    const [text, setText] = useState('');
-
+    // Add useEffect to initialize TypeIt after the component mounts
     useEffect(() => {
-        const originalText = "Hi, this is <span class='text-[#938000]'>JAKARIA</span> I am the master chief of London. Feel free to talk with me...";
-        let index = 0;
+        const simpleUsageElement = document.getElementById("simpleUsage");
 
-        const intervalId = setInterval(() => {
-            setText(originalText.slice(0, index));
-            index++;
-
-            if (index > originalText.length) {
-                clearInterval(intervalId);
-            }
-        }, 90);
-
-        return () => clearInterval(intervalId);
-    }, []);
+        if (simpleUsageElement) {
+            new TypeIt(simpleUsageElement, {
+                strings: "",
+                speed: 50,
+                waitUntilVisible: true,
+            }).go();
+        }
+    }, []); 
 
     return (
         <>
             <div className='lg:flex justify-between items-center px-[15%] h-screen gap-6'>
-
-                <div className='flex-1 floating-text mt-5 '>
-                    <div className='text-white lg:text-5xl text-xl font-mono  ' dangerouslySetInnerHTML={{ __html: text }} />
-
-                    <div className="flex text-white  items-center text-4xl mt-8 gap-5">
-                        <Link to="https://www.facebook.com/mdjakaria.j.r.143" target="blank"> <FaFacebook className="cursor-pointer" /></Link>
-                        <Link to="https://www.instagram.com/jakariaj.t23?igshid=MmVlMjlkMTBhMg==" target="blank"> <FaInstagram className="cursor-pointer" /></Link>
-                        <Link to="https://api.whatsapp.com/qr/5CKAPAVDVJ67P1?autoload=1&app_absent=0" target="blank"> <FaWhatsapp className="cursor-pointer" /></Link>
+                <div className='floating-text mt-5 h-[300px] space-y-20'>
+                    <p id="simpleUsage" className='text-white lg:text-5xl text-xl font-mono'>
+                        Hi, this is <span className='text-[#938000]'>JAKARIA</span>, Feel free to talk with me...
+                    </p>
+                    <div className="flex flex-col text-white justify-center items-center text-4xl mt-8 gap-5 space-y-14">
+                        <div className="flex gap-10">
+                            <Link to="https://www.facebook.com/mdjakaria.j.r.143" target="_blank">
+                                <FaFacebook className="cursor-pointer" />
+                            </Link>
+                            <Link to="https://www.instagram.com/jakariaj.t23?igshid=MmVlMjlkMTBhMg==" target="_blank">
+                                <FaInstagram className="cursor-pointer" />
+                            </Link>
+                            <Link to="https://api.whatsapp.com/qr/5CKAPAVDVJ67P1?autoload=1&app_absent=0" target="_blank">
+                                <FaWhatsapp className="cursor-pointer" />
+                            </Link>
+                        </div>
+                        <div>
+                        <button className="btn btn-neutral btn-outline px-10 text-[#938000] font-mono font-semibold">Download CV</button>
+                        </div>
                     </div>
-
                 </div>
-
-
-                <div className=''>
-                    <img className='w-[400px] h-fit flex-1' src={img} alt="" />
-                </div>
-
             </div>
         </>
     );
